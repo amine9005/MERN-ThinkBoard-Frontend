91c52519c62a2d5457657b3387d38c3c68a73ff3
amine9005 <amamselmitrader@gmail.com>
2025-11-09 00:55:05 +0100

    api update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
+  baseURL: "https://mern-thinkboard-backend-xvft.onrender.com/api/v1",
 });
 
 export default api;
